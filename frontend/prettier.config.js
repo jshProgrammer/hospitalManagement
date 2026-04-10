08b37d4dfbdf3eab3fcf87072cab539081aaba83
Tim <timknuettel@freenet.de>
2026-04-10 11:08:19 +0200

    feat(frontend): add basic react project
    a basic react project is built with vite, uses tailwind and typescript
    
    Signed-off-by: Tim <timknuettel@freenet.de>

---
+++ frontend/prettier.config.js
@@ -0,0 +1,20 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
+
+export default config
