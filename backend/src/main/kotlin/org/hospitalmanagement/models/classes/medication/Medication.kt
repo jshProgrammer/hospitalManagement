ac7dc1fe10603529979cfe9eca6bb7ce936fff80
Gottschalk125 <165592512+Gottschalk125@users.noreply.github.com>
2026-04-13 21:34:49 +0200

    Some DB Stuff, found some weird stuff we should talk about, else mainly finish the tables for medications, drugs and diagnosis. Also did some diagnosis cases

--- backend/src/main/kotlin/org/hospitalmanagement/models/classes/medication/Medication.kt
+++ backend/src/main/kotlin/org/hospitalmanagement/models/classes/medication/Medication.kt
@@ -1,11 +1,25 @@
 package org.hospitalmanagement.models.classes.medication
 
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import java.util.Date
+import jakarta.persistence.*
 
-data class Medication(
+@Entity
+@Table(name = "medication")
+class Medication(
+    @Id
     val id: Long,
-    val dosis: Dose,
+
+    @OneToOne
+    @JoinColumn(name = "dosis")
+    val dose: Dose,
+
+    @OneToOne
+    @JoinColumn(name = "drug")
     val drug: Drug,
+
     val started: Date?,
+
     val ended: Date?
 )
