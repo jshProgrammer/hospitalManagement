48809706345156a4e8c3e950eb4061dd89fdbf30
Tim <timknuettel@freenet.de>
2026-05-08 11:28:11 +0200

    feat(frontend): add patient types

---
+++ frontend/src/types/Patient.tsx
@@ -0,0 +1,41 @@
+export type Patient = {
+  id: number
+  personId: string
+  gender: string
+  firstName: string
+  lastName: string
+  plz: number
+  city: string
+  street: string
+  streetNo: number
+  country: string
+  birthday: string
+  phone: string
+  email: string
+}
+
+export type PatientPage = {
+  content: PatientApi[]
+  totalPages: number
+  totalElements: number
+  numberOfElements: number
+  size: number
+}
+
+export type PatientApi = {
+  id: number
+  person: {
+    id: string
+    gender: string
+    firstName: string
+    lastName: string
+    plz: number
+    city: string
+    street: string
+    streetNo: number
+    country: string
+    birthday: string
+    phone: string
+    email: string
+  }
+}
