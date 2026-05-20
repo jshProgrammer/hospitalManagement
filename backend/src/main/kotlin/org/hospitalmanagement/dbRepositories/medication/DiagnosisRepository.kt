7fb546a4573c39e3673f0a3e842b9397c0deff42
jshProgrammer <jpfennig2403@gmail.com>
2026-05-20 10:24:03 +0200

    (experimental): adding entity graph for diagnosis and medication
    Co-authored-by: xjasx4 <jasmin.wander04@gmail.com>

--- backend/src/main/kotlin/org/hospitalmanagement/dbRepositories/medication/DiagnosisRepository.kt
+++ backend/src/main/kotlin/org/hospitalmanagement/dbRepositories/medication/DiagnosisRepository.kt
@@ -1,14 +1,23 @@
 package org.hospitalmanagement.dbRepositories.medication
 
 import org.hospitalmanagement.models.classes.medication.Diagnosis
-import org.hospitalmanagement.models.classes.medication.Medication
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
 interface DiagnosisRepository : JpaRepository<Diagnosis, Long>, JpaSpecificationExecutor<Diagnosis> {
+
     fun findByDiagnosedPatientId(diagnosedPatientID: Long, pageable: Pageable): Page<Diagnosis>
+
+    @EntityGraph(attributePaths = [
+        "medication", "medication.dose", "medication.drug",
+        "diagnosedBy", "diagnosedBy.employee", "diagnosedBy.employee.person",
+        "diagnosedPatient", "diagnosedPatient.person"
+    ])
+    override fun findAll(spec: Specification<Diagnosis>?, pageable: Pageable): Page<Diagnosis>
 }
