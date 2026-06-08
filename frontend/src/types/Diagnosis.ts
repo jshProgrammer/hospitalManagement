ad329e1f9ec8e4249ce1892d7fd706d0e96796fe
Tim <timknuettel@freenet.de>
2026-06-08 20:02:42 +0200

    feat(frontend): add new types for individual patients-page

---
+++ frontend/src/types/Diagnosis.ts
@@ -0,0 +1,33 @@
+import type { PatientApi } from './Patient'
+import type { DoctorApi } from './Doctor.tsx'
+import type { DrugApi } from './Drug.tsx'
+
+export type DoseApi = {
+  id: number
+  unit: string
+  amount: number
+  frequency: string
+  frequencyAmount: number
+}
+
+export type MedicationApi = {
+  id: number
+  dose: DoseApi
+  drug: DrugApi
+  started: string
+  ended: string | null
+}
+
+export type DiagnosisApi = {
+  id: number
+  disease: string
+  medication: MedicationApi | null
+  diagnosedBy: DoctorApi
+  diagnosedPatient: PatientApi
+  diagnosedAt: string
+  diagnosedEnd: string | null
+}
+
+export type DiagnosesResponse = {
+  diagnoses: DiagnosisApi[]
+}
